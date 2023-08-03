653d5ed78a585f849bc4e987547d25036ee0382d
Chris Cox <chriscox@lsu.edu>
2023-08-03 16:56:14 -0500

    Fixed variable name typos

--- 03-smallworld_osg.R
+++ 03-smallworld_osg.R
@@ -13,17 +13,17 @@ str(list(subject_id, niter, network_path, vocab_vid_path, pos_counts_path, meta_
 
 library(igraph)
 
-pos_counts_df <- readRDS(file = pos_counts_path)[subject_id, ]
+pos_counts <- readRDS(file = pos_counts_path)[subject_id, ]
 vocab_vids <- readRDS(file = vocab_vid_path)[[subject_id]]
 g <- igraph::upgrade_graph(readRDS(file = network_path))
 m <- readRDS(file = metadata_path)
 
-print(pos_counts_df)
-pos_counts <- unlist(pos_counts[c("noun", "verb", "other")])
+print(pos_counts)
+n <- unlist(pos_counts[c("noun", "verb", "other")])
 
 results <- list(
     orig = swi(induced_subgraph(g, vids = vocab_vids)),
-    ran = replicate(niter, swi(balanced_ran(g, x$n, m$pos)), simplify = TRUE)
+    ran = replicate(niter, swi(balanced_ran(g, n, m$pos)), simplify = TRUE)
 )
 
 saveRDS(results, file = sprintf("%03d_smallworld.rds", subject_id))
